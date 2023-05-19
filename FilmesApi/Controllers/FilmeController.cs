d858ed454a3c78c478a3ec0af146fa215093f470
Diogo Silva <diogosilvaoficial@gmail.com>
2023-05-19 20:58:46 -0300

    Consultando e Paginando

--- FilmesApi/Controllers/FilmeController.cs
+++ FilmesApi/Controllers/FilmeController.cs
@@ -9,13 +9,30 @@ public class FilmeController : ControllerBase
 {
 
     private static List<Filme> filmes = new List<Filme>();
+    private static int id = 0;
 
     [HttpPost]
-    public void AdicionarFilme([FromBody] Filme filme)
+    public IActionResult AdicionarFilme([FromBody] Filme filme)
     {
+        filme.Id = id++;
         filmes.Add(filme);
-        Console.WriteLine(filme.Titulo);
-        Console.WriteLine(filme.Genero);
-        Console.WriteLine(filme.Duracao);
+        return CreatedAtAction(nameof(BuscarFilmePorId), new { id = filme.Id }, filme);
     }
+
+    [HttpGet]
+    public IEnumerable<Filme> BuscarFilmes(
+        [FromQuery] int page = 0, 
+        [FromQuery] int pageSize = 50)
+    {
+        return filmes.Skip(page).Take(pageSize);
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult BuscarFilmePorId(int id)
+    {
+        var filme = filmes.FirstOrDefault(filme => filme.Id == id);
+        if (filme == null) return NotFound();
+        return Ok(filme);
+    }
+
 }
