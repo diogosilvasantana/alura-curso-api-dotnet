0734ca98b8343d34ffd46475fde4d5f9c27c82f5
Diogo Silva <diogosilvaoficial@gmail.com>
2023-05-20 01:03:17 -0300

    Adicionando Dtos

--- FilmesApi/Program.cs
+++ FilmesApi/Program.cs
@@ -6,11 +6,9 @@ var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("FilmeConnection");
 
 builder.Services.AddDbContext<FilmeContext>(opts =>
-    opts.UseMySql(
-        connectionString,
-        ServerVersion.AutoDetect(connectionString)
-    )
-);
+    opts.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
+
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 // Add services to the container.
 
